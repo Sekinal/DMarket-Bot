c33949c94f761420338fefd749f4c494a3e13bf4
Thermostatic <muxxeroerty@gmail.com>
2025-01-28 14:54:54 -0600

    Debugging config

--- dashboard/app.py
+++ dashboard/app.py
@@ -25,7 +25,7 @@ def add_bot():
         api_url=data.get('api_url', "https://api.dmarket.com"),
         game_id=data.get('game_id', "a8db"),
         currency=data.get('currency', "USD"),
-        check_interval=int(data.get('check_interval', 960))
+        check_interval=int(data.get('check_interval', 30))
     )
     success = bot_manager.add_bot(data['instance_id'], config)
     return jsonify({'success': success})
